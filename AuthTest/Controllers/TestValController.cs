95bb39dce3f401de4b844af54d63dc32c6e3650f
tridip-bba <tridip@bba-reman.com>
2016-03-24 18:14:29 +0530

    add new view and controller

---
+++ AuthTest/Controllers/TestValController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AuthTest.Models;
+
+namespace AuthTest.Controllers
+{
+    public class TestValController : Controller
+    {
+        // GET: TestVal
+        public ActionResult Index()
+        {
+            var SampleVM = new SampleViewModel();
+            SampleVM.Products = new List<Product>
+            {
+                new Product{ ID=1, Name="IPhone" },
+                new Product{ ID=2, Name="MacBook Pro" },
+                new Product{ ID=3, Name="iPod" }           
+            };
+
+            SampleVM.Hobbies = new List<Hobby>()
+            {
+                new Hobby(){ Name = "Reading", IsSelected= false },
+                new Hobby(){ Name = "Sports" ,IsSelected= false},
+                new Hobby(){ Name = "Movies" ,IsSelected= false}
+            };
+            return View(SampleVM);
+        }
+
+        [HttpPost]
+        public ActionResult Index(SampleViewModel vm)
+        {
+            ViewBag.IsPostBack = false;
+
+            if (ModelState.IsValid)
+            {
+                Product p = vm.Products.Where(x => x.ID == vm.SelectedProductId).FirstOrDefault();
+                ViewBag.IsPostBack = true;
+                ViewBag.ProductID = p.ID.ToString();
+                ViewBag.ProductName = p.Name.ToString();
+                return View(vm);
+            }
+            else
+            {
+                var SampleVM = new SampleViewModel();
+                SampleVM.Products = new List<Product>
+                {
+                    new Product{ ID=1, Name="IPhone" },
+                    new Product{ ID=2, Name="MacBook Pro" },
+                    new Product{ ID=3, Name="iPod" }           
+                };
+                SampleVM.Hobbies = new List<Hobby>()
+                {
+                    new Hobby(){ Name = "Reading", IsSelected= false },
+                    new Hobby(){ Name = "Sports" ,IsSelected= true},
+                    new Hobby(){ Name = "Movies" ,IsSelected= true}
+                };
+
+                return View(SampleVM);
+            }
+        }
+    }
+}
---
+++ AuthTest/Models/SampleModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace AuthTest.Models
+{
+    public class Product
+    {
+        public int ID { set; get; }
+        public string Name { set; get; }
+    }
+
+    public class Hobby
+    {
+        public string Name { get; set; }
+        public bool IsSelected { get; set; }
+    }
+
+    public class SampleViewModel
+    {
+        [Display(Name = "Products")]
+        public List<Product> Products { set; get; }
+
+        [Required(ErrorMessage = "Select any Hobbies")]
+        public List<Hobby> Hobbies { get; set; }
+
+        [Required(ErrorMessage = "Select any Product")]
+        public int SelectedProductId { set; get; }
+
+        [Required(ErrorMessage = "Select Male or Female")]
+        public string Gender { get; set; }
+    }
+}
